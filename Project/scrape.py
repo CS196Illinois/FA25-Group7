be92e4c8be82074bd6925654d6f72f935e26c888
Dawid <dawidwas155@gmail.com>
2025-10-08 19:06:40 -0500

    Fix State Farm Scraper

--- Project/scrape.py
+++ Project/scrape.py
@@ -2,39 +2,12 @@
 from bs4 import BeautifulSoup
 import requests
 import json
-<<<<<<< HEAD
+import re
+from playwright.sync_api import sync_playwright 
 from datetime import datetime, timedelta
 
 # Variables & Constants
-NUM_EVENTS = 1
-events = {}
-no_events = []
-
-"""
-TOOO:
-
--> IGNORE:
-http://senate.illinois.edu/a_calendar.asp
-https://extension.illinois.edu/global/events
-http://illinois.edu/calendar/list/642
-
--> MAKE NEW SCRAPERS FOR:
-http://www.thestatefarmcenter.com/
-http://www.fightingillini.com/calendar.aspx
-
--> Make JSON consistent for each event
-
--> Add a tag feature in the JSON
-
--> Make different scrapers for each type of calendar (General, State Farm, Sports)
-
--> Clean up code and add more reusability
-"""
-=======
-import re
-
-# Variables & Constants
-GENERAL_CALENDARS = [
+GENERAL_CALENDAR_LINKS = [
     "https://calendars.illinois.edu/list/7",
     "https://calendars.illinois.edu/list/557",
     "https://calendars.illinois.edu/list/594",
@@ -46,9 +19,8 @@ GENERAL_CALENDARS = [
     "https://calendars.illinois.edu/list/4757",
     "https://calendars.illinois.edu/list/598"
 ]
-STATE_FARM_CENTER_CALENDAR = "https://www.statefarmcenter.com/events/all"
-ATHLETIC_CALENDAR = "https://fightingillini.com/calendar"
->>>>>>> 192aa6d95ee484b67954eb8ea418e7f538197294
+STATE_FARM_CENTER_CALENDAR_LINK = "https://www.statefarmcenter.com/events/all"
+ATHLETIC_CALENDAR_LINK = "https://fightingillini.com/calendar"
 
 # Functions
 def get_general_event_info(link: str):
@@ -75,33 +47,15 @@ def get_general_event_info(link: str):
     if desc != None:
         event_info["description"] = desc.text
 
+    # Cost of the event, assume to be 0 unless otherwise specified
+    event_info["cost"] = 0.0
+    
     # The rest of the details are stored in a dl, convert dt's and dd's into a dictionary
     details = dict(zip(
                 [detail.text.strip().lower().replace(" ", "_") for detail in event.find_all("dt")], 
                 [detail.text for detail in event.find_all("dd")]
                 ))
-<<<<<<< HEAD
                 
-    # Put each detail into our event_info dict
-    for key in details:
-        match key:
-            case "date":
-                # Splits up the date and time since they are in one string
-                date_time = details[key].split("\xa0")
-                # Adds the date/range of dates
-                event_info["date"] = date_time[0].strip()
-                # Converts the given time slot into start_time and end_time
-                time = date_time[1].strip()
-                if time != "All Day" and time != "":
-                    time = time.split(" ")
-                    if len(time) >= 5:
-                        event_info["start_time"] = time[0] + " " + time[4].upper()
-                        event_info["end_time"] = time[3] + " " + time[4].upper()
-=======
-    
-    # Cost of the event, assume to be 0 unless otherwise specified
-    event_info["cost"] = 0.0
-    
     # Put each detail into our event_info dict, matching the format of our JSON
     for key in details:
         match key:
@@ -112,7 +66,6 @@ def get_general_event_info(link: str):
                     event_info["start_date"] = match_date.group(1)
                     if match_date.group(2) is not None:
                         event_info["end_date"] = match_date.group(2)
->>>>>>> 192aa6d95ee484b67954eb8ea418e7f538197294
                     else:
                         event_info["end_date"] = match_date.group(1)
                 else:
@@ -145,126 +98,118 @@ def get_general_event_info(link: str):
                 event_info["host"] = details[key]
             case "price" | "cost":
                 event_info["cost"] = details[key]
+
+    # Cleanup
+    for key in event_info:
+        event_info[key] = event_info[key].strip()
     
     return event_info
 
-<<<<<<< HEAD
-    # Converts dict object into json format and returns it        
-    return json.dumps(event_info, indent=4, sort_keys=True)
-
 def get_state_farm_center_event_info(link: str):
     # Scrapes the html from the event page
-    html_text = requests.get(link).text
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page(user_agent="Mozilla/5.0")
+        page.goto(link, wait_until="networkidle")
+        html_text = page.content()
+        browser.close()
     soup = BeautifulSoup(html_text, "lxml")
-    event = soup.find("div", class_="leftColumn")
+    
     # Dictonary to store event info
     event_info = {}
+    
     # Name of the event
-    event_info["title"] = event.find("h1").text
+    event_info["title"] = soup.find("h1", class_="title").text
+    
     # Description for the event, if given
     event_info["description"] = ""
-    desc = event.find("div", class_="description_inner")
+    desc = soup.find("div", class_="description_inner").find_all("p")
     if desc != None:
-        event_info["description"] = desc.text
-    # The rest of the details are stored in a dl, convert dt's and dd's into a dictionary
-    details = dict(zip(
-                [detail.text.strip().lower().replace(" ", "_") for detail in event.find_all("div")], 
-                [detail.text for detail in event.find_all("span")]
-                ))
-    for key in details:
-        match key:
-            case "Date":
-                # Adds the date
-                event_info["date"] = details[key].strip()
-            case "Event Starts":
-                # Finds start time
-                start_time = details[key]
-                # Adds the start time
-                event_info["start_time"] = start_time.strip()
-                start_time_obj = datetime.strptime(event_info['start_time'], "%I:%M %p")
-                end_time_obj = start_time_obj + timedelta(hours=2)
-                event_info['end_time'] = end_time_obj.strftime("%I:%M %p")
+        event_info["description"] = " ".join([text.text for text in desc])
 
-            case _:
-                event_info[key] = details[key]
+    # Hard-Coded data, same for all events
+    event_info["location"] = "State Farm Center 1800 S 1st St, Champaign, IL 61820"
+    event_info["tag"] = "Entertainment"
+    event_info["host"] = "State Farm Center"
 
+    # Sidebar data
+    sidebar = soup.find("ul", class_="eventDetailList") 
+    event_info["start_date"] = sidebar.find("span", class_="m-date__month").text + sidebar.find("span", class_="m-date__day").text + sidebar.find("span", class_="m-date__year").text
+    event_info["end_date"] = event_info["start_date"]
+    event_info["start_time"] = sidebar.find("li", class_="item sidebar_event_starts").find("span").text.strip()
+    event_info["end_time"] = (datetime.strptime(event_info['start_time'], "%I:%M %p") + timedelta(hours=3)).strftime("%I:%M %p")
+    event_info["cost"] = sidebar.find("li", class_="item sidebar_ticket_prices").find("span").text
 
+    # Cleanup
+    for key in event_info:
+        event_info[key] = event_info[key].strip()
 
-def main():
-    # Scrapes the main calendar page
-    html_text = requests.get("https://illinois.edu/resources/calendars.html").text
-    soup = BeautifulSoup(html_text, "lxml")
-    calendar_list = soup.find("ul", class_="ruled")
-    calendars = calendar_list.find_all("li")
+    return event_info
 
-    for calendar in calendars:
-=======
+def get_athletic_event_info(link: str):
+    # TODO: Make custom scraper for athletic events
+    pass
+
 def scrape_general():
     # Scrapes General Events
     events = {}
-    for calendar_link in GENERAL_CALENDARS:
->>>>>>> 192aa6d95ee484b67954eb8ea418e7f538197294
+    for calendar_link in GENERAL_CALENDAR_LINKS:
         # Scrapes the calendar page
         html_text = requests.get(calendar_link).text
         soup = BeautifulSoup(html_text, "lxml")
-        if link == "http://www.thestatefarmcenter.com/":
-            event_listings = soup.find_all("h3", class_="title")
-            title_tag = "State Farm Center"
-        else:
-            event_listings = soup.find_all("div", class_="title")
-            # Prints calendar name & link
-            title_tag = soup.find("h1")
+        event_listings = soup.find_all("div", class_="title")
 
-<<<<<<< HEAD
-        # Skips calendar if there are no events
-        if len(event_listings) == 0:
-            no_events.append(link)
-            continue
-
-        # Checking if title provided
-        if type(title_tag) != str:
-            calendar_title = title_tag.text.strip()
-        else:
-            if title_tag:
-                calendar_title = title_tag
-            else:
-                calendar_title = "Unknown Calendar Title"
-
-        print(f"CALENDAR: {calendar_title} ({link}) \n")
-
-        # Prints the first NUM_EVENTS events from the calendar, if any exist
-        for i in range(0, min(len(event_listings), NUM_EVENTS)):
-            event_link = "https://calendars.illinois.edu/" + event_listings[i].find("a").attrs["href"]
-            # Handles links that don't work with general scraper
-            if link == "http://www.thestatefarmcenter.com/":
-                event_data = get_state_farm_center_event_info(event_link)
-            else:
-                event_data = get_uiuc_event_info(event_link)
-            if event_data:
-                events[event_link] = event_data
-                print(f"Event #{str(i + 1)}: {event_link}")
-                print(event_data, end="\n\n")
-
-        print("-" * 100)
-=======
-        # Puts the event link and event info into the JSON for each event in the calendar
+        # Calls the function for general events using each link in the event_listings
         for i in range(0, len(event_listings)):
             event_link = "https://calendars.illinois.edu/" + event_listings[i].find("a").attrs["href"]
             events[event_link] = get_general_event_info(event_link)
 
-    return events
->>>>>>> 192aa6d95ee484b67954eb8ea418e7f538197294
-
 def scrape_state_farm():
-    pass
+    events = {}
+    # Scrapes State Farm Events
+    html_text = requests.get(STATE_FARM_CENTER_CALENDAR_LINK).text
+    soup = BeautifulSoup(html_text, "lxml")
+    event_listings = soup.find_all("a", class_="more buttons-hide")
+
+    # Calls the function for State Farm Center events using each link in the event_listings
+    for i in range(0, len(event_listings)):
+        event_link = event_listings[i].attrs["href"]
+        events[event_link] = get_state_farm_center_event_info(event_link)
+
+    return events
 
 def scrape_athletics():
-    pass
+    events = {}
+    # Scrapes Athletic Events
+    html_text = requests.get(ATHLETIC_CALENDAR_LINK).text
+    soup = BeautifulSoup(html_text, "lxml")
+    event_listings = [] # TODO: Use soup to get a list of all event links
+
+    # Calls the function for Athletic events using each link in the event_listings
+    for i in range(0, len(event_listings)):
+        event_link = event_listings[i].attrs["href"]
+        events[event_link] = get_athletic_event_info(event_link)
+    
+    return events
 
 def main():
-    json_data_general = scrape_general()
-    with open("general_events.json", "w", encoding="utf-8") as file:
-        json.dump(json_data_general, file, indent=4, ensure_ascii=False)
+    scrape_gen = input("Do you want to scrape General events? (y/n) ")
+    if scrape_gen == "y":
+        json_data_general = scrape_general()
+        with open("general_events.json", "w", encoding="utf-8") as file:
+            json.dump(json_data_general, file, indent=4, ensure_ascii=False)
 
+    scrape_sf = input("Do you want to scrape State Farm Center events? (y/n) ")
+    if scrape_sf == "y":
+        json_data_state_farm = scrape_state_farm()
+        with open("state_farm_events.json", "w", encoding="utf-8") as file:
+            json.dump(json_data_state_farm, file, indent=4, ensure_ascii=False)
+
+    scrape_ath = input("Do you want to scrape Athletic events? (y/n) ")
+    if scrape_ath == "y":
+        json_data_athletics = scrape_athletics()
+        with open("athletic_events.json", "w", encoding="utf-8") as file:
+            json.dump(json_data_athletics, file, indent=4, ensure_ascii=False)
+    
 if __name__ == "__main__":
     main()
